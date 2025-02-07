e1fa39162f7c3d0cae3866172464269aa46852f9
reassDev <reassDev@proton.me>
2025-02-07 11:02:33 +0100

    Added a few helper functions to IslandUtils

--- README.md
+++ README.md
@@ -11,3 +11,15 @@
 ReassClient is a 1.8.9 Hypixel SkyBlock QoL Mod. It is currently under development.
 
 The configuration menu is currently using OneConfig and can be accessed through there or through '/rc'.
+
+# Features
+
+## Send Vanguard Profit to Discord Webhook
+Sends your profit from Vanguards to a Discord Webhook
+REQUIRES: SKYHANNI CORPSE PROFIT CALCULATOR
+
+## Death Alert
+Sends a message in party chat when someone in your instance dies
+
+## Simple Party Commands
+Party Commands that have !warp, !coords, !pt, and !allinvite
--- src/main/java/io/github/reassdev/ReassClient/utils/IslandUtils.java
+++ src/main/java/io/github/reassdev/ReassClient/utils/IslandUtils.java
@@ -1,2 +1,57 @@
-package io.github.reassdev.ReassClient.utils;public class IslandUtils {
+package io.github.reassdev.ReassClient.utils;
+
+import cc.polyfrost.oneconfig.libs.eventbus.Subscribe;
+import net.minecraft.client.Minecraft;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+
+public class IslandUtils {
+
+    boolean isOnHypixel = false;
+    boolean isOnSkyBlock = false;
+
+    String islandName = "Unknown";
+
+    int dungeonFloor = 0;
+    int kuudraTier = 0;
+
+    @Subscribe
+    public void onDisconnect(FMLNetworkEvent.ClientDisconnectionFromServerEvent e) {
+        isOnHypixel = false;
+        isOnSkyBlock = false;
+        islandName = "Unknown";
+        dungeonFloor = 0;
+        kuudraTier = 0;
+    }
+
+    @Subscribe
+    public void onWorldChange(WorldEvent.Unload e) {
+        isOnSkyBlock = false;
+        islandName = "Unknown";
+        dungeonFloor = 0;
+        kuudraTier = 0;
+    }
+
+    @Subscribe
+    public void onConnect(FMLNetworkEvent.ClientConnectedToServerEvent e) {
+        if(!e.isLocal && Minecraft.getMinecraft().thePlayer.getClientBrand().contains(("hypixel")) && Minecraft.getMinecraft().getCurrentServerData().serverIP.contains("hypixel")) {
+            isOnHypixel = true;
+        }
+    }
+
+    public Scoreboard getScoreboard() {
+        return Minecraft.getMinecraft().thePlayer.getWorldScoreboard();
+    }
+
+    // TODO: Make functions here using scoreboard:
+    public void getIsland() {
+
+    }
+
+    private void isOnSkyBlockChecker() {
+
+    }
+
+
 }
